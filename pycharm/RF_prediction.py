c6dd60d469739b1631fb5cc68b1e753eca4fd671
yyenglee <lee2yy@mail.uc.edu>
2021-05-27 14:10:40 -0400

    update RFprediction.py

--- pycharm/RF_prediction.py
+++ pycharm/RF_prediction.py
@@ -46,7 +46,7 @@ weight = (10000/labeldata[labeldata["label"] == "SRG"].shape[0])
 all_indices = list(range(X.shape[0]))
 feature_list = dataset.columns[2:]
 seed_value = 18
-nStep = 10
+nStep = 100
 SUFFIX = "n" + str(nStep) + "_" + DATE + "_"
 
 featureDict = {}
@@ -58,6 +58,7 @@ for i in range(len(testRatio)):
     sss = StratifiedShuffleSplit(n_splits=nStep, test_size=testRatio[i], random_state=seed_value)
     print(curRatio)
 
+    new_weight = weight * (1.2-testRatio[i])
     genepredictTableRFpos = pd.DataFrame(0, index=sleepGeneList, columns=sleepGeneList)
     genepredictTableRFneg = pd.DataFrame(0, index=sleepGeneList, columns=sleepGeneList)
 
@@ -66,55 +67,45 @@ for i in range(len(testRatio)):
 
     j = 0
     for train_index, test_index in sss.split(X, y_rename):
-        print(j, curRatio)
         j += 1
-
+        X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y_rename[train_index], y_rename[test_index]
 
-        ptrain_index = train_index[y_train==1]
-        ntrain_index = train_index[y_train==0]
+        testcase = np.array([1 if i in test_index else 0 for i in range(len(y))])
+        traintesttable.insert(2, "testcase_"+str(j), testcase, True)
 
-        for k in range(20):
-            ctrain_index = list(ptrain_index) + random.sample(list(ntrain_index), 1000)
-            ctest_index = list(set(range(0,len(y_rename)))-set(ctrain_index))
+        trainSleepGene = list(dataset.loc[train_index, "GeneSymbol"][y_train == 1])
+        testSleepGene = list(dataset.loc[test_index, "GeneSymbol"][y_test == 1])
 
-            X_train, X_test = X[ctrain_index], X[ctest_index]
-            y_train, y_test = y_rename[ctrain_index], y_rename[ctest_index]
+        name = "RandomForest"
+        rf = RandomForestClassifier(max_leaf_nodes=12, class_weight={1: weight, 0: 1})
+        rf.fit(X_train, y_train)
+        predictions = rf.predict_proba(X_test)
+        y_RF = rf.predict_proba(X)
+        y_predictSRG = [round(i[1], 4) for i in y_RF]
+        RFtable.insert(2, "pred__"+str(j), y_predictSRG, True)
 
-            testcase = np.array([1 if i in test_index else 0 for i in range(len(y))])
-            traintesttable.insert(2, "testcase_"+str(j), testcase, True)
+        # feature importances
+        importances = list(rf.feature_importances_)
+        # List of tuples with variable and importance
+        feature_importances = [(feature, round(importance, 4)) for feature, importance in zip(feature_list, importances)]
+        # Sort the feature importances by most important first
+        feature_importances = sorted(feature_importances, key=lambda x: x[1], reverse=True)
 
-            trainSleepGene = list(dataset.loc[ctrain_index, "GeneSymbol"][y_train == 1])
-            testSleepGene = list(dataset.loc[ctest_index, "GeneSymbol"][y_test == 1])
-
-            name = "RandomForest"
-            rf = RandomForestClassifier(max_leaf_nodes=12, class_weight={1: weight, 0: 1})
-            rf.fit(X_train, y_train)
-            predictions = rf.predict_proba(X_test)
-            y_RF = rf.predict_proba(X)
-            y_predictSRG = [round(i[1], 4) for i in y_RF]
-            RFtable.insert(2, "pred_"+str(j)+"_"+str(k), y_predictSRG, True)
-
-            # feature importances
-            importances = list(rf.feature_importances_)
-            # List of tuples with variable and importance
-            feature_importances = [(feature, round(importance, 4)) for feature, importance in zip(feature_list, importances)]
-            # Sort the feature importances by most important first
-            feature_importances = sorted(feature_importances, key=lambda x: x[1], reverse=True)
-
-            # Record features have effect >0
-            for ll in range(len(feature_importances)):
-                if feature_importances[ll][1] > 0:
-                    try:
-                        featureDict[feature_importances[ll][0]][testRatio[i]].append(feature_importances[ll][1])
-                    except KeyError:
-                        featureDict[feature_importances[ll][0]][testRatio[i]] = [feature_importances[ll][1]]
+        # Record features have effect >0
+        for k in range(len(feature_importances)):
+            if feature_importances[k][1] > 0:
+                try:
+                    featureDict[feature_importances[k][0]][testRatio[i]].append(feature_importances[k][1])
+                except KeyError:
+                    featureDict[feature_importances[k][0]][testRatio[i]] = [feature_importances[k][1]]
 
     traintesttable.to_csv(PATH + "traintesttable_" + SUFFIX + str(testRatio[i])+".csv")
     RFtable.to_csv(PATH + "RFtable_" + SUFFIX + str(testRatio[i])+".csv")
 
 wf = open(PATH + "feature_importance_" + DATE + ".txt", "w")
 for f in featureDict.keys():
+    #wf.write('%s\n' % f)
     for j in featureDict[f].keys():
         wf.write('%s\t%s\t'%(f,j))
         for k in featureDict[f][j]:
